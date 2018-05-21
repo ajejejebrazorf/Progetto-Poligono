bd18f9af1ee2caf42da07379bde7319e49763daf
ajejejebrazorf <37294646+ajejejebrazorf@users.noreply.github.com>
2018-05-21 15:07:44 +0200

    Lil Update

--- src/Test/NodoTest.java
+++ src/Test/NodoTest.java
@@ -10,13 +10,20 @@ import Main.Nodo;
 public class NodoTest {
 	
 	Accesso accesso = new Accesso();
+	Accesso accesso2 = new Accesso();
 
 	@Test
 	public void testNodo() throws Exception 
 	{
 		Nodo nodo = new Nodo(accesso);
+		Nodo nodo2 = new Nodo(accesso);		
+		
 		assertEquals(nodo.getInfo(), accesso);
 		assertEquals(nodo.getLink(), null);
+		nodo.setInfo(accesso2);
+		nodo.setLink(nodo2);
+		assertEquals(nodo.getInfo(), accesso2);
+		assertEquals(nodo.getLink(), nodo2);
 	}
 
 	@Test
@@ -24,13 +31,18 @@ public class NodoTest {
 	{
 		Nodo nodo = new Nodo(accesso);
 		assertEquals(nodo.getInfo(), accesso);
+		nodo.setInfo(accesso2);
+		assertEquals(nodo.getInfo(), accesso2);
 	}
 
 	@Test
 	public void testGetLink() throws Exception 
 	{
 		Nodo nodo = new Nodo(accesso);
+		Nodo nodo2 = new Nodo(accesso);
 		assertEquals(nodo.getLink(), null);
+		nodo.setLink(nodo2);
+		assertEquals(nodo.getLink(), nodo2);
 	}
 
 }
